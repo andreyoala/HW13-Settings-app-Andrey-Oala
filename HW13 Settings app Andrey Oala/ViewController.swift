fb9397d3c54d5f7a71e07f2ea733415825eea983
Andrey <andreyoala@icloud.com>
2022-06-02 13:02:45 +0300

    Initial Commit

---
+++ HW13 Settings app Andrey Oala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW13 Settings app Andrey Oala
+//
+//  Created by Andrey Oala on 02.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
