3d98049c45efdeaa07d036712f8122120ce343a6
Andrey <andreyoala@icloud.com>
2022-06-03 17:25:36 +0300

    Cleaned the code and added marks

--- HW13 Settings app Andrey Oala/SwitchTableViewCell.swift
+++ HW13 Settings app Andrey Oala/SwitchTableViewCell.swift
@@ -1,8 +1,8 @@
 import UIKit
 
-class SettingsTableViewCell: UITableViewCell {
+class SwitchTableViewCell: UITableViewCell {
 
-    static let indentifier = "SettingsTableViewCell"
+    static let indentifier = "SwitchTableViewCell"
     
     private let iconContainer: UIView = {
         let view = UIView()
@@ -24,14 +24,21 @@ class SettingsTableViewCell: UITableViewCell {
         label.numberOfLines = 1
         return label
     }()
-
+    
+    private let mySwitch: UISwitch = {
+       let mySwitch = UISwitch()
+       mySwitch.onTintColor = .systemGreen
+       return mySwitch
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super .init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(label)
         contentView.addSubview(iconContainer)
+        contentView.addSubview(mySwitch)
         iconContainer.addSubview(iconImageView)
         contentView.clipsToBounds = true
-        accessoryType = .disclosureIndicator
+        accessoryType = .none
     }
     required init?(coder: NSCoder) {
         fatalError()
@@ -45,21 +52,29 @@ class SettingsTableViewCell: UITableViewCell {
                                      y: (size-imageSize)/2,
                                      width: imageSize,
                                      height: imageSize)
+        mySwitch.sizeToFit()
+        mySwitch.frame = CGRect(x: contentView.frame.size.width - mySwitch.frame.size.width - 20,
+                                y: (contentView.frame.size.height - mySwitch.frame.size.height) / 2,
+                                width: mySwitch.frame.size.width,
+                                height: mySwitch.frame.size.height)
         
         label.frame = CGRect(x: 25 + iconContainer.frame.size.width,
                              y: 0,
                              width: contentView.frame.size.width - 20 - iconContainer.frame.size.width,
                              height: contentView.frame.size.height)
     }
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         iconImageView.image = nil
         label.text = nil
         iconContainer.backgroundColor = nil
+        mySwitch.isOn = false
     }
-    public func configure(with model: SettingsOption) {
+    public func configure(with model: SettingsSwitchOption) {
         label.text = model.title
         iconImageView.image = model.icon
         iconContainer.backgroundColor = model.iconBackgroundColor
+        mySwitch.isOn = model.isOn
     }
 }
--- HW13 Settings app Andrey Oala/ViewController.swift
+++ HW13 Settings app Andrey Oala/ViewController.swift
@@ -1,7 +1,20 @@
 import UIKit
 
 struct Section {
-    let options: [SettingsOption]
+    let options: [SettingsOptionType]
+}
+
+enum SettingsOptionType {
+    case staticCell(model: SettingsOption)
+    case switchCell(model: SettingsSwitchOption)
+}
+
+struct SettingsSwitchOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+    var isOn: Bool
 }
 
 struct SettingsOption {
@@ -11,113 +24,138 @@ struct SettingsOption {
     let handler: (() -> Void)
 }
 
-
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var models = [Section]()
+    private let tableVIew: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.indentifier)
+        table.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.indentifier)
+        return table
+    }()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models[section].options.count
     }
-    
-    func configureCells() {
-        models.append(Section(options: [
-            SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemOrange) {
-            },
-            SettingsOption(title: "Wi-Fi", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .systemBlue) {
-            },
-            SettingsOption(title: "Bluetooth", icon: UIImage(systemName: "point.3.connected.trianglepath.dotted"), iconBackgroundColor: .systemBlue) {
-                
-            },
-            SettingsOption(title: "Cellular", icon: UIImage(systemName: "antenna.radiowaves.left.and.right"), iconBackgroundColor: .systemGreen) {
-                
-            }
-        ]))
-        models.append(Section(options: [
-            SettingsOption(title: "Notifications", icon: UIImage(systemName: "app.badge.fill"), iconBackgroundColor: .systemRed) {
-                
-            },
-            SettingsOption(title: "Sounds & Haptics", icon: UIImage(systemName: "speaker.wave.3"), iconBackgroundColor: .systemPink) {
-                
-            },
-            SettingsOption(title: "Focus", icon: UIImage(systemName: "moon.fill"), iconBackgroundColor: .systemIndigo) {
-                
-            },
-            SettingsOption(title: "Screen Time", icon: UIImage(systemName: "hourglass"), iconBackgroundColor: .systemIndigo) {
-                
-            }
-        ]))
-        models.append(Section(options: [
-            SettingsOption(title: "General", icon: UIImage(systemName: "gear"), iconBackgroundColor: .systemGray) {
-                
-            },
-            SettingsOption(title: "Control Center", icon: UIImage(systemName: "switch.2"), iconBackgroundColor: .systemGray) {
-                
-            },
-            SettingsOption(title: "Display & Brightness", icon: UIImage(systemName: "textformat.size"), iconBackgroundColor: .systemBlue) {
-                
-            },
-            SettingsOption(title: "Home Screen", icon: UIImage(systemName: "app.badge.fill"), iconBackgroundColor: .systemIndigo) {
-                
-            },
-            SettingsOption(title: "Accessibility", icon: UIImage(systemName: "figure.wave.circle"), iconBackgroundColor: .systemBlue) {
-                
-            }
-            ,
-            SettingsOption(title: "Wallpapers", icon: UIImage(systemName: "photo.fill"), iconBackgroundColor: .systemTeal) {
-                
-            },
-            SettingsOption(title: "Siri & Search", icon: UIImage(systemName: "h.square.on.square.fill"), iconBackgroundColor: .black) {
-                
-            }
-            
-            ]))
-
-    }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let model = models[indexPath.section].options[indexPath.row]
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.indentifier,
-                                                 for: indexPath) as? SettingsTableViewCell else {
-            return UITableViewCell()
+        
+        switch model.self {
+        case .staticCell(let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.indentifier,
+            for: indexPath) as? SettingsTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
+        case .switchCell(let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SwitchTableViewCell.indentifier,
+            for: indexPath) as? SwitchTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
         }
-        cell.configure(with: model)
-        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return models.count
     }
-    
-
-    private let tableVIew: UITableView = {
-        let table = UITableView(frame: .zero, style: .grouped)
-        table.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.indentifier)
-        return table
-    }()
-    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
-        configureCells()
         super.viewDidLoad()
-        title = "Settings"
+        configureCells()
         view.addSubview(tableVIew)
         tableVIew.delegate = self
         tableVIew.dataSource = self
         tableVIew.frame = view.bounds
+        title = "Settings"
     }
-
-
 }
+
 
 extension ViewController {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let model = models[indexPath.section].options[indexPath.row]
-            let text = model.title
-                print("Нажата ячейка \(text)")
-        model.handler()
+        let type = models[indexPath.section].options[indexPath.row]
+        switch type.self {
+        case .staticCell(let model):
+            model.handler()
+            print("Нажата ячейка \(model.title)")
+        case .switchCell(let model):
+            model.handler()
+            print("Нажата ячейка \(model.title)")
+        }
     }
 }
+
+var models = [Section]()
+// MARK: - Strings
+enum Strings {
+    static let airplaneModeTitle = "Airplane Mode"
+    static let wifiTitle = "Wi-Fi"
+    static let bluetoothTitle = "Bluetooth"
+    static let cellularTitle = "Cellular"
+    static let notificationsTitle = "Notifications"
+    static let soundsAndHapticsTitle = "Sounds & Haptics"
+    static let focusModeTitle = "Focus"
+    static let screenTimeTitle = "Screen Time"
+    static let generalOptionsTitle = "General"
+    static let controlCenterTitle = "Control Center"
+    static let displayAndBrightnessTitle = "Display & Brightness"
+    static let homeScreenTitle = "Home Screen"
+    static let accessibilityTitle = "Accessibility"
+    static let wallpapersTitle = "Wallpapers"
+    static let siriAndSearchTitle = "Siri & Search"
+}
+
+// MARK: - Icons
+enum Icons {
+    static let airplaneModeIcon = "airplane"
+    static let wifiIcon = "wifi"
+    static let bluetoothIcon = "point.3.connected.trianglepath.dotted"
+    static let cellularIcon = "antenna.radiowaves.left.and.right"
+    static let notificationsIcon = "app.badge.fill"
+    static let soundsAndHapticsIcon = "speaker.wave.3"
+    static let focusModeIcon = "moon.fill"
+    static let screenTimeIcon = "hourglass"
+    static let generalOptionsIcon = "gear"
+    static let controlCenterIcon = "switch.2"
+    static let displayAndBrightnessIcon = "textformat.size"
+    static let homeScreenIcon = "app.badge.fill"
+    static let accessibilityIcon = "figure.wave.circle"
+    static let wallpapersIcon = "photo.fill"
+    static let siriAndSearchIcon = "h.square.on.square.fill"
+}
+    
+// MARK: - Configuring Cells
+    func configureCells() {
+        models.append(Section(options: [
+            .switchCell(model: SettingsSwitchOption(title: Strings.airplaneModeTitle, icon: UIImage(systemName: Icons.airplaneModeIcon), iconBackgroundColor: .systemOrange, handler: {
+            }, isOn: true)),
+            .staticCell(model: SettingsOption(title: Strings.wifiTitle, icon: UIImage(systemName: Icons.wifiIcon), iconBackgroundColor: .systemBlue) {}),
+            .staticCell(model: SettingsOption(title: Strings.bluetoothTitle, icon: UIImage(systemName: Icons.bluetoothIcon), iconBackgroundColor: .systemBlue) {}),
+            .staticCell(model: SettingsOption(title: Strings.cellularTitle, icon: UIImage(systemName: Icons.cellularIcon), iconBackgroundColor: .systemGreen) {})
+        ]))
+        
+        models.append(Section(options: [
+            .staticCell(model: SettingsOption(title: Strings.notificationsTitle, icon: UIImage(systemName: Icons.notificationsIcon), iconBackgroundColor: .systemRed) {}),
+            .staticCell(model: SettingsOption(title: Strings.soundsAndHapticsTitle, icon: UIImage(systemName: Icons.soundsAndHapticsIcon), iconBackgroundColor: .systemPink) {}),
+            .staticCell(model: SettingsOption(title: Strings.focusModeTitle, icon: UIImage(systemName: Icons.focusModeIcon), iconBackgroundColor: .systemIndigo) {}),
+            .staticCell(model: SettingsOption(title: Strings.screenTimeTitle, icon: UIImage(systemName: Icons.screenTimeIcon), iconBackgroundColor: .systemIndigo) {})
+        ]))
+        
+        models.append(Section(options: [
+            .staticCell(model: SettingsOption(title: Strings.generalOptionsTitle, icon: UIImage(systemName: Icons.generalOptionsIcon), iconBackgroundColor: .systemGray) {}),
+            .staticCell(model: SettingsOption(title: Strings.controlCenterTitle, icon: UIImage(systemName: Icons.controlCenterIcon), iconBackgroundColor: .systemGray) {}),
+            .staticCell(model: SettingsOption(title: Strings.displayAndBrightnessTitle, icon: UIImage(systemName: Icons.displayAndBrightnessIcon), iconBackgroundColor: .systemBlue) {}),
+            .staticCell(model: SettingsOption(title: Strings.homeScreenTitle, icon: UIImage(systemName: Icons.homeScreenIcon), iconBackgroundColor: .systemIndigo) {}),
+            .staticCell(model: SettingsOption(title: Strings.accessibilityTitle, icon: UIImage(systemName: Icons.accessibilityIcon), iconBackgroundColor: .systemBlue) {}),
+            .staticCell(model: SettingsOption(title: Strings.wallpapersTitle, icon: UIImage(systemName: Icons.wallpapersIcon), iconBackgroundColor: .systemTeal) {}),
+            .staticCell(model: SettingsOption(title: Strings.siriAndSearchTitle, icon: UIImage(systemName: Icons.siriAndSearchIcon), iconBackgroundColor: .black) {})
+            ]))
+}
+
+
 
 
